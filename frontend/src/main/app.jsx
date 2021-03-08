6ea2742e4c50dfebfbe1720a7eb5874974687283
lyusk8 <lyusk8@gmail.com>
2021-03-08 00:25:30 -0300

    criado pasta frontend e configuração webpack

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import 'modules/bootstrap/dist/bootstrap.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
+import React from 'react';
+//import Todo from '../todo/todo';
+
+export default (props) => (
+  <div>
+    <h1>Teste</h1>
+  </div>
+);
